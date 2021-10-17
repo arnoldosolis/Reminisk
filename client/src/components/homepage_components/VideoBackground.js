d537f4ff6457cd04dc797af5b49f9240bc069c8d
Ivan Nicolas Junio <ivannicolas.junio83@myhunter.cuny.edu>
2021-10-16 21:48:57 -0400

    Made homepage folder for organization

---
+++ client/src/components/homepage_components/VideoBackground.js
@@ -0,0 +1,23 @@
+import { Button } from "../Button"
+import "./VideoBackground.css"
+
+function VideoBackground() {
+    return (
+        <div className="video-container">
+            <video className="video" src="../videos/video-1.mp4" autoPlay loop muted />
+            <h1>Reminisk</h1>
+            <p>Where your mental health is a priority.</p>
+            
+            <div className="video-btns">
+                <Button className="btns" buttonStyle='btn--outline' buttonSize="btn--large" linkTo="/journal">
+                    Log Journal Entry
+                </Button>
+                <Button className="btns" buttonStyle='btn--primary' buttonSize="btn--large" linkTo="/survey">
+                    Fill Out Survey
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default VideoBackground;
