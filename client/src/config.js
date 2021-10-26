e4a3f718b2b6cc04d383fe376a5c87e72d471d6a
Ivan Nicolas Junio <ivannicolas.junio83@myhunter.cuny.edu>
2021-10-26 15:30:00 -0400

    Google API keys

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+const MAPS_API_KEY = "AIzaSyCKTiKzLSpkhGO_v1h_jGq6CltajbkrskM"
+const GEO_API_KEY = "AIzaSyDqOcVXL0A62nUYZwVe4lieQFFzyPlWqx8"
+const PLACES_API_KEY = "AIzaSyA_fsUld2U0c2h67rhk4ZgYxgQdQSWk1yc"
+export {
+    MAPS_API_KEY, GEO_API_KEY, PLACES_API_KEY
+};
