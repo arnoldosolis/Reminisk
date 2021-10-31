96e781d1ce48ac79cb81e7c68fb9304dc8b71132
Ivan Nicolas Junio <ivannicolas.junio83@myhunter.cuny.edu>
2021-10-31 10:44:49 -0400

    added new testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,24 @@
-import "./App.css";
-import { Route, Switch } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegistrationPage from "./pages/RegistrationPage";
-import Navbar from "./components/navbar_components/Navbar";
-import Footer from "./components/footer_components/Footer";
-import VideoBackground from "./components/homepage_components/VideoBackground";
-import Survey from "./components/surveypage_components/Survey";
-import ScrollToTop from "./components/ScrollToTop";
-import JournalPage from "./pages/JournalPage";
-import { LoginContext } from "./Helper/Context"
-import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
   return (
-    <LoginContext.Provider value={{ loggedIn, setLoggedIn}}>
-      <Switch>
-        <Route path="/" exact>
-          <LoginPage />
-        </Route>
-        <Route path="/registration">
-          <RegistrationPage />
-        </Route>
-        <ScrollToTop>
-          <>
-            <Navbar />
-            <Route exact path="/home" component={() => <VideoBackground authorized={loggedIn} />} />
-            <Route exact path="/survey" component={() => <Survey authorized={loggedIn} />} />
-            <Route exact path="/journal">
-              <JournalPage authorized={loggedIn} />
-            </Route>
-            {/* <Route exact path="/mapit" component{}/> */}
-            {/* <Route exact path="/profile" component{}/> */}
-            <Footer />
-          </>
-        </ScrollToTop>
-      </Switch>
-    </LoginContext.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
