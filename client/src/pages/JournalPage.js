45ab5d2db5c0d23f10a7529e27dc055a7932763c
Judy <judy.zhang67@myhunter.cuny.edu>
2021-11-01 19:19:37 -0400

    moving maps to pages

--- client/src/pages/JournalPage.js
+++ client/src/pages/JournalPage.js
@@ -16,28 +16,6 @@ import { Redirect } from "react-router-dom";
 const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let labelIndex = 0;
 
-/*
-function Map() {
-  const bangalore = { lat: 12.97, lng: 77.59 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
-    center: bangalore,
-  });
-  google.maps.event.addListener(map, "click", (event) => {
-    addMarker(event.latLng, map);
-  });
-  addMarker(bangalore, map);
-}
-
-// Adds a marker to the map.
-function addMarker(location, map) {
-
-  new google.maps.Marker({
-    position: location,
-    label: labels[labelIndex++ % labels.length],
-    map: map,
-  });
-} */
 
 function JournalPage({ authorized }) {
   const [buttonPopup, setButtonPopup] = useState(false);
---
+++ client/src/pages/map.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {GoogleApiWrapper, Map, Marker, InfoWindow} from  'google-maps-react'
+import './map.css'
+import { Component } from 'react'
+
+export class MapC extends Component{
+
+  onMarkerDragEnd = (coord, index) => {
+    const { latLng } = coord;
+    const lat = latLng.lat();
+    const lng = latLng.lng();
+
+    this.setState(prevState => {
+      const markers = [...this.state.markers];
+      markers[index] = { ...markers[index], position: { lat, lng } };
+      return { markers };
+    });
+  }
+  render(){
+    return(
+
+      <Map google = {this.props.google}
+      zoom ={13}
+      style = {{
+        width:"100%",
+        height: "50%"
+      }}
+      center = {{lat: 12, lng :-75}}>
+    \
+
+        <Marker position ={{lat: 12, lng :-75}}
+        onDragEnd = {this.moveMarker.bind(this)}
+        draggable = {true}
+        name= {"Marker"}/>
+
+            <InfoWindow onClose={this.onInfoWindowClose}>
+              <div>
+                <h2>{this.state.selectedPlace.name}</h2>
+              </div>
+            </InfoWindow>
+      </Map>
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey:'AIzaSyCjexxgkitg1lfaWOqau0OIOW0OYBCgrTc'
+}) (MapC)
