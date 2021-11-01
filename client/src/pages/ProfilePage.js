483dd7489325b88dd615cf6c8e334e8c45580465
Ivan Nicolas Junio <ivannicolas.junio83@myhunter.cuny.edu>
2021-11-01 12:56:23 -0400

    Added new editing feature

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -9,6 +9,8 @@ function ProfilePage({ authorized }) {
     const [email, setEmail] = useState("")
     const [savedFacilities, setSavedFacilities] = useState([])
 
+    const [edit, setEdit] = useState(false)
+    const [newEmail, setNewEmail] = useState("")
     useEffect(() => {
         Axios.get("http://localhost:3001/userinfo").then((response) => {
             setName(response.data[0].name);
@@ -18,6 +20,24 @@ function ProfilePage({ authorized }) {
             setSavedFacilities(response.data)
         });
     }, []);
+
+    Axios.defaults.withCredentials = true;
+    const editInfo = () => {
+        Axios.put("http://localhost:3001/updateEmail", {
+            new_email: newEmail
+        }).then((response) => {
+            console.log("Result: ", response);
+        },
+            (error) => {
+                console.error("Error:", error);
+            }
+        )
+        Axios.get("http://localhost:3001/userinfo").then((response) => {
+            setEmail(response.data[0].email);
+        });
+        setEdit(false)
+    }
+
     if (!authorized) {
         return <Redirect to="/" />;
     }
@@ -27,31 +47,44 @@ function ProfilePage({ authorized }) {
                 <h1 className="profile-hdr">Profile Page</h1>
                 <br />
                 <div className="userinfo-cntr">
-                    <h4>User Information</h4>
+                    <h4 className="userinfo-hdr">User Information</h4>
                     <label className="username-lbl">Name: {name}</label>
-                    <label className="useremail-lbl">Email: {email}</label>
+                    {edit ?
+                        <p className="edit-p">
+                            <label className="edit-lbl">Edit Email:</label>
+                            <input className="update-inp" placeholder="Input New Email" onChange={(e) => {
+            setNewEmail(e.target.value)}}/>
+                            <button className="confirm-btn" onClick={editInfo}>Confirm Edit</button>
+                            <button className="cancel-btn" onClick={() => setEdit(false)}>Cancel Edit</button>
+                        </p>
+                        :
+                        <p>
+                            <label className="useremail-lbl">Email: {email}</label>
+                            <button className="edit-btn" onClick={() => setEdit(true)}>Edit Information</button>
+                        </p>
+                    }
                 </div>
                 <div className="facilityinfo-cntr">
-                <h4 className="facilityinfo-hdr">Saved Facility Information</h4>
-                {savedFacilities.length !== 0 ?
-                savedFacilities.map((val, index) => (
-                    <div className="facilityinfo-cntr" key={index}>
-                        <label className="facility-lbl">Facility Name: {val.facility_name!==null ? val.facility_name : "N/A"}</label>
-                        <label className="facility-lbl">Facility Address: {val.facility_address!==null ? val.facility_address : "N/A"}</label>
-                        <label className="facility-lbl">Facility Phone: {val.facility_phone!==null ? val.facility_phone : "N/A"}</label>
-                        <label className="facility-lbl">Facility Times: 
-                        {val.facility_times !== null ?  val.facility_times.split(',').map((times, index) => (
-                            <label className="hours-p" key={index}>{times}</label>
+                    <h4 className="facilityinfo-hdr">Saved Facility Information</h4>
+                    {savedFacilities.length !== 0 ?
+                        savedFacilities.map((val, index) => (
+                            <div className="facilityinfo-cntr" key={index}>
+                                <label className="facility-lbl">Facility Name: {val.facility_name !== null ? val.facility_name : "N/A"}</label>
+                                <label className="facility-lbl">Facility Address: {val.facility_address !== null ? val.facility_address : "N/A"}</label>
+                                <label className="facility-lbl">Facility Phone: {val.facility_phone !== null ? val.facility_phone : "N/A"}</label>
+                                <label className="facility-lbl">Facility Times:
+                                    {val.facility_times !== null ? val.facility_times.split(',').map((times, index) => (
+                                        <label className="hours-p" key={index}>{times}</label>
+                                    ))
+                                        : "N/A"}
+                                </label>
+                            </div>
+
+
                         ))
-                        : "N/A" }
-                        </label>
-                    </div>
-                    
-                    
-                ))
-                : 
-                    <h2>No Facilities Saved</h2>
-                }
+                        :
+                        <h2>No Facilities Saved</h2>
+                    }
                 </div>
             </div>
         </div>
