ac644e2848abeb6e7d5a58828ebf941057261af8
Judy <judy.zhang67@myhunter.cuny.edu>
2021-11-16 00:10:02 -0500

    mapit.js

---
+++ client/src/components/mapit_components/mapit.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { useLocation } from 'react-router';
+import { useState, useEffect, useRef } from 'react';
+//import { MAPS_API_KEY } from '../../config'
+import { getDetails } from 'use-places-autocomplete';
+//import Confirm from "./confirmPopup";
+import Axios from 'axios'
+import { LoadScript, useLoadScript, GoogleMap, Marker, InfoWindow, Maps, StandaloneSearchBox} from "@react-google-maps/api";
+const MapContainer = ({ array, isAdding, getLocation }) => {
+
+  const [ selected, setSelected ] = useState({});
+  const [ currentPosition, setCurrentPosition ] = useState({});
+
+  const markerRef = useRef(null);
+
+  const defaultCenter = {
+    lat: 40.7128, lng: 74.0060
+  }
+
+  const onSelect = item => {
+    setSelected(item);
+  }
+
+  const success = (position) => {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const currentPosition = {
+      lat: latitude,
+      lng: longitude
+    }
+    setCurrentPosition(currentPosition);
+  }
+
+  const onMarkerDragEnd = (e) => {
+    const lat = e.latLng.lat();
+    const lng = e.latLng.lng();
+    setCurrentPosition({ lat, lng})
+  };
+
+  const footer = (
+    <div className="footer">
+      <div className="inner-footer">
+      <span className="location-text">Choose location and press</span>
+      <Button variant="contained" color="primary" onClick={() => getLocation(currentPosition)}>
+        Next
+      </Button>
+      </div>
+    </div>
+  );
+
+  const mapStyles = () => {
+    if (!isAdding) {
+      return {
+        marginTop: "-20px",
+        height: "100vh",
+        width: "100%"
+      }
+    } else {
+      return {
+        marginTop: "-20px",
+        height: "80vh",
+        width: "100%"
+      }
+    }
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  })
+
+     return (
+    <>
+      <LoadScript
+        id="script-loader"
+        googleMapsApiKey='AIzaSyCKTiKzLSpkhGO_v1h_jGq6CltajbkrskM'
+      >
+        <GoogleMap
+          id='example-map'
+          mapContainerStyle={mapStyles()}
+          draggable={true}
+          zoom={13}
+          center={currentPosition.lat ? currentPosition : defaultCenter}
+        >
+          {
+            array ?
+            array.map(item => {
+              return (
+              <Marker
+              key={item.id}
+              position={item.location}
+              onClick={() => onSelect(item)}
+              />
+              )
+            }) : null
+          }
+          {
+            isAdding ?
+            <Marker
+            position={currentPosition}
+            ref={() => markerRef}
+            onDragEnd={(e) => onMarkerDragEnd(e)}
+            draggable={true} /> :
+            null
+          }
+          {
+            selected.location ?
+            (
+              <InfoWindow
+              position={selected.location}
+              onCloseClick={() => setSelected({})}
+            >
+            </InfoWindow>
+            ) : null
+          }
+        </GoogleMap>
+      </LoadScript>
+      {
+        isAdding ?
+        footer :
+        null
+      }
+    </>
+     )
+  }
+
+export default MapContainer;
