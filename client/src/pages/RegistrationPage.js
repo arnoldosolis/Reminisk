f1a1e219cbab20c59c4a07ce7be89ced79c7a83d
Huiyang Lin <www.bloodsugar@gmail.com>
2021-11-27 21:28:30 -0500

    added password requirements

--- client/src/pages/RegistrationPage.js
+++ client/src/pages/RegistrationPage.js
@@ -1,5 +1,4 @@
 import styles from "./RegistrationPage.module.css";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import Axios from "axios";
 import Modal from "../components/Modal";
@@ -12,68 +11,7 @@ function RegistrationPage() {
   const [email, setEmail] = useState("");
   const [regSuccessModal, setRegSuccessModal] = useState(false);
   let history = useHistory();
-  
-  var passwordInput = document.getElementById("password");
-  var letter = document.getElementById("letter");
-  var capital = document.getElementById("capital");
-  var number = document.getElementById("number");
-  var length = document.getElementById("length");
-  
-  //When the user clicks on the password field, show the message box
-  passwordInput.onfocus = function() {
-  document.getElementById("message").style.display = "block";
-  }
 
-  //When the user clicks off of the password field, hide the message box
-  passwordInput.onblur = function() {
-    document.getElementById("message").style.display = "none";
-  }
-
-  //When the user starts to type something inside the password field, validate certain requirements
-  passwordInput.onkeyup = function() {
-    //Validate lowercase letters
-    var lowercaseLetters = /[a-z]/g;
-    if(passwordInput.value.match(lowercaseLetters)){
-      letter.classList.remove("invalid");
-      letter.classList.add("valid");
-    }
-    else {
-      letter.classList.remove("valid");
-      letter.classList.add("invalid");
-    }
-    
-    //Validate capital letters
-    var capitalLetters = /[A-Z]/g;
-    if(passwordInput.value.match(capitalLetters)){
-      capital.classList.remove("invalid");
-      capital.classList.add("valid");
-    }
-    else {
-      capital.classList.remove("valid");
-      capital.classList.add("invalid");
-    }
-
-    //Validate numbers
-    var numbers = /[0-9]/g;
-    if(passwordInput.value.match(numbers)){
-      number.classList.remove("invalid");
-      number.classList.add("valid");
-    }
-    else {
-      number.classList.remove("valid");
-      number.classList.add("invalid");
-    }
-
-    //Validate length
-    if(passwordInput.value.length >= 8){
-      length.classList.remove("invalid");
-      length.classList.add("valid");
-    }
-    else {
-      length.classList.remove("valid");
-      length.classList.add("invalid");
-    }
-  };
   function addUser (event) {
     //prevents page from refreshing after form submission
     event.preventDefault();
@@ -138,6 +76,7 @@ function RegistrationPage() {
             pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
             title="Must contain at least one number, one lowercase letter, one uppercase letter, and must be at least 8 or more characters"
             placeholder="Enter Password"
+            maxLength="255"
             required
             onChange={(event) => {
               setPassword(event.target.value);
@@ -156,15 +95,7 @@ function RegistrationPage() {
           />
           <button>Join Reminisk</button>
         </form>
-        {regSuccessModal && <Modal onClick={redirectToLogin} display={"Registration Successful!"} />}
-
-        <div id="message">
-          <p id="letter" class="invalid">r</p>
-          <p id="capital" class="invalid"></p>
-          <p id="number" class="invalid"></p>
-          <p id="length" class="invalid"></p>
-        </div>
-        
+        {regSuccessModal && <Modal onClick={redirectToLogin} display={"Registration Successful!"} />}   
       </div> 
   );
 }
