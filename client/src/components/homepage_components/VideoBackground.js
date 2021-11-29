efdfbdd8f7b55f6bce9a758614989ff03f99e377
Judy <judy.zhang67@myhunter.cuny.edu>
2021-11-29 09:06:14 -0500

    button

--- client/src/components/homepage_components/VideoBackground.js
+++ client/src/components/homepage_components/VideoBackground.js
@@ -7,13 +7,16 @@ function VideoBackground() {
             <video className="video" src="../videos/video-1.mp4" autoPlay loop muted />
             <h1>Reminisk</h1>
             <p>Where your mental health and well-being is a priority.</p>
-            
+
             <div className="video-btns">
                 <Button className="btns" buttonStyle='btn--outline' buttonSize="btn--large" linkTo="/journal">
                     Log Journal Entry
                 </Button>
                 <Button className="btns" buttonStyle='btn--primary' buttonSize="btn--large" linkTo="/survey">
                     Fill Out Survey
+                </Button>
+                <Button className="btns" buttonStyle='btn--primary' buttonSize="btn--large" linkTo="/mapit">
+                    Map It
                 </Button>
             </div>
         </div>
