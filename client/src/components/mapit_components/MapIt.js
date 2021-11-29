ec97de245b7c91b79f3e12a7c661edc637f98e11
Judy <judy.zhang67@myhunter.cuny.edu>
2021-11-29 09:03:57 -0500

    mapit

---
+++ client/src/components/mapit_components/MapIt.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './MapIt.css';
+import MyGoogleMap from './MyGoogleMap';
+
+
+function MapIt() {
+
+  return (
+    <div className="main-wrapper">
+      <MyGoogleMap />
+    </div>
+  );
+}
+
+export default MapIt;
