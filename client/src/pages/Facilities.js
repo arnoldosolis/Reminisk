e2d199fd70b1c7a7462879f4e03d812d02b4aa3d
Ivan Nicolas Junio <ivannicolas.junio83@myhunter.cuny.edu>
2021-11-30 13:06:33 -0500

    Added pagination to profile page

--- client/src/pages/Facilities.js
+++ client/src/pages/Facilities.js
@@ -29,26 +29,6 @@ function Facilities( { facilities, deleteFacility, editFacility }) {
                             </i>
                         </div>
                         : ""}
-                    {/* <label className="facility-lbl">Facility Name: {facility.facility_name !== null ? facility.facility_name : "N/A"}</label>
-            <label className="facility-lbl">Facility Address: {facility.facility_address !== null ? facility.facility_address : "N/A"}</label>
-            {show ?
-                <>
-                    <label className="facility-lbl">Facility Phone: {facility.facility_phone !== null ? facility.facility_phone : "N/A"}</label>
-                    <label className="facility-lbl">Facility Times:
-                        {facility.facility_times !== null ? facility.facility_times.split(',').map((times, index) => (
-                            <label className="hours-p" key={index}>{times}</label>
-                        ))
-                            : "N/A"}
-                    </label>
-                </> : ""}
-            <button className="show-btn" onClick={() => { setShow(!show) }}>{!show ? "Show More" : "Show Less"}</button>
-            {editFacility === true ?
-
-                <div className="delete-icon" onClick={() => { deleteFacility(facility.facility_id) }}>
-                    <i className="fas fa-minus-square" >
-                    </i>
-                </div>
-                : ""} */}
                 </div>
             ))}
         </>
