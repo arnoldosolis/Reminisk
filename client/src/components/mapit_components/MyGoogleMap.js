07e7d3af20ab7e51fc2196c61d0e919b826126fd
Arnoldo Solis <arnoldosolis817@gmail.com>
2021-12-09 02:18:47 -0500

    almost

--- client/src/components/mapit_components/MyGoogleMap.js
+++ client/src/components/mapit_components/MyGoogleMap.js
@@ -130,24 +130,16 @@ class MyGoogleMap extends Component {
     });
   }
 
-  geocode() {
+  geocode(location) {
     var location = "22 Main st Boston MA";
-    let tmp = [];
-    this.state.journalList.map((data) => {
-      console.log(data.location);
-      Axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
-        params: {
-          address: location,
-          key: "AIzaSyCKTiKzLSpkhGO_v1h_jGq6CltajbkrskM",
-        },
-      }).then((response) => {
-        console.log(response.data.results[0].geometry.lat);
-        tmp.push({
-          id: data.journallog_id,
-          lat: response.data.results[0].geometry.lat,
-          lng: response.data.results[0].geometry.lat,
-        });
-      });
+    Axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        address: location,
+        key: "AIzaSyCKTiKzLSpkhGO_v1h_jGq6CltajbkrskM",
+      },
+    }).then((response) => {
+      console.log(response.data.results[0].geometry.location.lat);
+      console.log(response.data.results[0].geometry.location.lng);
     });
   }
   mapMarkers = () => {
